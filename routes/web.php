d5904d285f43610400026994fefd8267cd5c4428
SrOliver202023 <sroliver202023@gmail.com>
2023-07-04 19:48:50 -0300

    fix: suppliers routes

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,6 @@ use Inertia\Inertia;
 
 use App\Http\Controllers\SupplierController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Auth/Login', [
@@ -39,8 +29,6 @@ Route::get('/products', function () {
 })->middleware(['auth', 'verified'])->name('products');
 
 
-Route::resource('suppliers', SupplierController::class)
-->only(['index','store','show','edit','update'])
-->middleware('auth','verified');
+Route::resource('suppliers', SupplierController::class)->middleware('auth','verified');
 
 require __DIR__.'/auth.php';
