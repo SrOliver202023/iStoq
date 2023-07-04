c72e58fd930199bd2c33299a63f98f11a89055b3
SrOliver202023 <sroliver202023@gmail.com>
2023-07-04 10:33:19 -0300

    chore: suppliers types single added

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -10,3 +10,55 @@ export type PageProps<T extends Record<string, unknown> = Record<string, unknown
         user: User;
     };
 };
+
+
+export type Supplier = {
+    id: number
+    nameSupplier: string
+    email: string
+    cnpj: string
+    phone: string
+    address: string
+}
+
+export type SupplierDTO = {
+    nameSupplier: string
+    email: string
+    cnpj: string
+    phone: string
+    address: string
+}
+
+export type SuppliersProps = {
+    suppliers: Supplier[]
+}
+export type SupplierProps = {
+    supplier: Supplier
+}
+
+export type SuppliersPaginationProps = {
+    suppliers: Pagination<Supplier>
+}
+
+export type Pagination<T> = {
+    current_page: number;
+    data: T[]
+    first_page_url: string
+    from: number
+    last_page: number
+    last_page_url: string
+    links: Link[]
+    next_page_url: string
+    path: string
+    per_page: number
+    prev_page_url: any
+    to: number
+    total: number
+}
+
+export interface Link {
+    url?: string
+    label: string
+    active: boolean
+}
+
